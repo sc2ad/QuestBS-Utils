9a286b6615481966cf03219f62e64ba18cb06d9e
Adam ? <adamznow@gmail.com>
2020-09-17 12:20:46 -0700

    Override name
    I'm actually not sure if the version change will break the module name still, it might.

--- Android.mk
+++ Android.mk
@@ -26,19 +26,19 @@ LOCAL_SRC_FILES := extern/libmodloader.so
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: beatsaber-hook - version: 0.4.7
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_0_5_4
+LOCAL_MODULE := beatsaber-hook_0_5_9
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_0_5_4.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_0_5_9.so
 LOCAL_CPP_FEATURES += rtti exceptions
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := bs-utils_0_3_3
+LOCAL_MODULE := bs-utils
 LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook/,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_0_5_4
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_0_5_9
 LOCAL_LDLIBS += -llog
 LOCAL_CFLAGS += -DVERSION='"0.3.3"' -isystem 'extern/libil2cpp/il2cpp/libil2cpp' -DID='"bs-utils"' -I'./shared' -I'./extern'
 LOCAL_CPPFLAGS += -std=c++2a -frtti
