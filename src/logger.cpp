25caf1caf775f6b04b8d85493ba4fd746b6cb1be
Adam ? <adamznow@gmail.com>
2020-10-26 21:47:19 -0700

    Remove broken file logging

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,6 +1,6 @@
 #include "utils-logging.hpp"
 
 const Logger& getLogger() {
-    static const Logger logger = Logger(ModInfo{ID, VERSION}, LoggerOptions(false, true));
+    static const Logger logger = Logger(ModInfo{ID, VERSION});
     return logger;
 }
